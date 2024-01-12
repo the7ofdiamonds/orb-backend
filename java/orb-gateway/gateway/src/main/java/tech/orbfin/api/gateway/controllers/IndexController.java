240161cd6cf68a2af673596a9fa2697d11786bdf
Jamel Lyons <32249473+the7ofdiamonds@users.noreply.github.com>
2024-01-11 21:59:05 -0500

    Added support for JWT

--- java/orb-gateway/gateway/src/main/java/tech/orbfin/api/gateway/controllers/IndexController.java
+++ java/orb-gateway/gateway/src/main/java/tech/orbfin/api/gateway/controllers/IndexController.java
@@ -1,23 +1,12 @@
 package tech.orbfin.api.gateway.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/")
+@RequestMapping
 public class IndexController {
 
-    @GetMapping
+    @GetMapping("/")
     public String index() {
         return "index";
-    }
-
-    @CrossOrigin
-    @PostMapping
-    public ResponseEntity<String> checkJwt(@RequestBody String jwt) {
-            return new ResponseEntity<>(jwt, HttpStatus.OK);
     }
 }
---
+++ java/orb-gateway/gateway/src/main/java/tech/orbfin/api/gateway/token/TokenType.java
@@ -0,0 +1,5 @@
+package tech.orbfin.api.gateway.token;
+
+public enum TokenType {
+    BEARER
+}
