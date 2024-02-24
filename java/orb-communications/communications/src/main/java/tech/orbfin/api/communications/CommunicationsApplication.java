375885cabc1caaa44cbcf2fa01595712e3dab58f
Jamel Lyons <32249473+the7ofdiamonds@users.noreply.github.com>
2024-02-24 15:01:50 -0500

    Added communications container for email and notifications

---
+++ java/orb-communications/communications/src/main/java/tech/orbfin/api/communications/CommunicationsApplication.java
@@ -0,0 +1,13 @@
+package tech.orbfin.api.communications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicationsApplication.class, args);
+	}
+
+}
