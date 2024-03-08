93f35284df17fb4e5df2752ac60e4e5d624d6e3c
Jamel Lyons <32249473+the7ofdiamonds@users.noreply.github.com>
2024-03-08 16:10:32 -0500

    Added stored procedures

---
+++ java/orb-gateway/gateway/src/main/resources/ExpireAccount.sql
@@ -0,0 +1,30 @@
+CREATE DEFINER=`root`@`%` PROCEDURE `expireAccount`(
+    IN p_user_email VARCHAR(255), 
+    IN p_display_name VARCHAR(255), 
+	IN p_confirmation_code VARCHAR(255)
+)
+BEGIN
+    DECLARE user_id INT;
+
+SELECT 
+    u.ID
+INTO user_id FROM
+    wordpress.wp_users u
+        LEFT JOIN
+    wordpress.wp_usermeta m ON u.ID = m.user_id
+WHERE
+    u.user_email COLLATE utf8mb4_unicode_520_ci = p_user_email
+        AND u.display_name COLLATE utf8mb4_unicode_520_ci = p_display_name
+		AND m.meta_key = 'confirmation_code'
+		AND m.meta_value COLLATE utf8mb4_unicode_520_ci = p_confirmation_code;
+        
+    IF user_id IS NOT NULL THEN
+        CALL addUserMeta(user_id, 'is_account_non_expired', false);
+    END IF;
+    
+	SELECT 
+    CASE
+        WHEN ROW_COUNT() > 0 THEN 'TRUE'
+        ELSE 'FALSE'
+    END AS resultSet;
+END
