950037d71c7902aaa580d8ca92d1e79b8cc7aceb
Jamel Lyons <32249473+the7ofdiamonds@users.noreply.github.com>
2024-03-13 16:20:31 -0400

    Refactoring 3/13/24 @ 16:20

--- java/orb-gateway/gateway/src/main/java/tech/orbfin/api/gateway/utils/PHP.java
+++ java/orb-gateway/gateway/src/main/java/tech/orbfin/api/gateway/utils/PHP.java
@@ -28,23 +28,164 @@ public class PHP {
             return null;
         }
 
-        if (sdata.equals("i:") && scIndex == -1) {
+        if (sdata.equals("i:") && scIndex == -1 && fbIndex == -1 && lbIndex == -1) {
             return (T) uInteger(sdata);
         }
 
-        if (sdata.equals("d:") && scIndex == -1) {
-            return (T) uFloat(sdata);
+        if (sdata.equals("d:") && scIndex == -1 && fbIndex == -1 && lbIndex == -1) {
+            return (T) uDouble(sdata);
         }
 
         if (sdata.equals("b:") && sdataLength == 3) {
             return (T) uBoolean(sdata);
         }
 
-        if (sdata.equals("s:") && scIndex != -1) {
+        if (sdata.equals("s:") && scIndex != -1 && fbIndex == -1 && lbIndex == -1) {
             return (T) uString(sdata);
         }
 
-        if (sdata.startsWith("a:") || sdata.startsWith("O:")) {
+        if (sdata.startsWith("a:")) {
+            return (T) uArray(sdata);
+        }
+
+        if (sdata.startsWith("O:")) {
+            return (T) uObject(sdata);
+        }
+
+        return null;
+    }
+
+    public Integer uInteger(String sdata) {
+        try {
+            if (!sdata.startsWith("i:")) {
+                return null;
+            }
+
+            int fSemiIndex = sdata.indexOf(';');
+            String integerValue = sdata.substring(2, fSemiIndex);
+
+            return Integer.parseInt(integerValue);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Double uDouble(String sdata) {
+        try {
+            if (!sdata.startsWith("d:")) {
+                return null;
+            }
+
+            int fSemiIndex = sdata.indexOf(';');
+            String doubleValue = sdata.substring(2, fSemiIndex);
+
+            return Double.parseDouble(doubleValue);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Boolean uBoolean(String sdata) {
+        if (!sdata.startsWith("b:")) {
+            return null;
+        }
+
+        if (sdata.equals("b:1;")) {
+            return true;
+        }
+
+        if (sdata.equals("b:0;")) {
+            return false;
+        }
+
+        return null;
+    }
+
+    public static String uString(String sdata) {
+        int fColIndex = sdata.indexOf(':');
+        int sColIndex = sdata.indexOf(':', fColIndex +1);
+        int chars = Integer.parseInt(sdata.substring(fColIndex + 1, sColIndex));
+        int fSemiIndex = sdata.indexOf(';');
+
+        String data = sdata.substring(sColIndex + 2, fSemiIndex - 1); // Adjust substring indices to exclude quotes
+
+        int dataLength = data.length();
+
+        Pattern pattern = Pattern.compile("[^a-z]");
+        Matcher matcher = pattern.matcher(data);
+
+        if (matcher.find()) {
+            System.out.println("Invalid characters found in the extracted data.");
+        }
+
+        if (chars != dataLength){
+            System.out.println("There has been an error number of characters expected " + chars + " and length of string "+ dataLength + " should be the same.");
+        }
+
+        return data;
+    }
+
+    public static List<String> splitOutsideCurlyBraces(String input, String delimiter) {
+        List<String> parts = new ArrayList<>();
+        Pattern pattern = Pattern.compile("(?![^{]*\\})(?=" + Pattern.quote(delimiter) + ")");
+        Matcher matcher = pattern.matcher(input);
+        int start = 0;
+        while (matcher.find()) {
+            String part = input.substring(start, matcher.end());
+            log.info(part.trim());
+            parts.add(part.trim());
+            start = matcher.end();
+        }
+        if (start < input.length()) {
+            parts.add(input.substring(start).trim());
+        }
+        return parts;
+    }
+
+    private <T> T uArray(String sdata) {
+        int sdataLength = sdata.length();
+        int fbIndex = sdata.indexOf('{');
+        int lbIndex = sdata.indexOf('}');
+        int fcIndex = sdata.indexOf(':');
+        int scIndex = sdata.indexOf(':', fcIndex + 1);
+
+        if (sdata.startsWith("a:")) {
+            int items = Integer.parseInt(sdata.substring(fcIndex + 1, scIndex));
+            String data = sdata.substring(fbIndex + 1, lbIndex);
+            log.info(String.valueOf(items));
+
+            Collection<T> array = new ArrayList<>(items);
+
+            if (items > 1) {
+                List<String> newDataList = splitOutsideCurlyBraces(data, ";{");
+                var newDataListSize = newDataList.size();
+
+                for (String newData : newDataList) {
+                    if (data.startsWith("s:")) {
+                        var s = (T) uString(newData);
+                        array.add(s);
+                    }
+                }
+
+                log.info(String.valueOf(newDataListSize));
+            }
+
+            return (T) array;
+        }
+
+        return null;
+    }
+
+    private <T> T uObject(String sdata) {
+        int sdataLength = sdata.length();
+        int fbIndex = sdata.indexOf('{');
+        int lbIndex = sdata.indexOf('}');
+        int fcIndex = sdata.indexOf(':');
+        int scIndex = sdata.indexOf(':', fcIndex + 1);
+
+        if (sdata.startsWith("O:")) {
             int items = Integer.parseInt(sdata.substring(fcIndex + 1, scIndex));
             String data = sdata.substring(fbIndex + 1, lbIndex);
             log.info(String.valueOf(items));
@@ -70,79 +211,5 @@ public class PHP {
         }
 
         return null;
-    }
-
-    private Integer uInteger(String sdata) {
-        if (sdata.startsWith("i:")) {
-            String integerValue = sdata.substring(2);
-            try {
-                return Integer.parseInt(integerValue);
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-        return null;
-    }
-
-    private Double uFloat(String sdata) {
-        if (sdata.startsWith("d:")) {
-            String floatValue = sdata.substring(2);
-            try {
-                return Double.parseDouble(floatValue);
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-        return null;
-    }
-
-    private Boolean uBoolean(String sdata) {
-        if (sdata.equals("b:1")) {
-            return true;
-        }
-
-        if (sdata.equals("b:0")) {
-            return false;
-        }
-
-        return null;
-    }
-
-    private String uString(String sdata) {
-        int fcIndex = sdata.indexOf(':');
-        int scIndex = sdata.indexOf(':', fcIndex + 1);
-        int chars = Integer.parseInt(sdata.substring(fcIndex + 1, scIndex));
-        int fComaIndex = sdata.indexOf('"');
-        int lComaIndex = sdata.lastIndexOf('"');
-        String data = sdata.substring(fComaIndex, lComaIndex);
-        int dataLength = data.length();
-        log.info(String.valueOf(chars));
-        log.info(String.valueOf(dataLength));
-        if (chars != dataLength) {
-            log.info("true");
-        }
-
-        return data;
-    }
-
-    public static List<String> splitOutsideCurlyBraces(String input, String delimiter) {
-        List<String> parts = new ArrayList<>();
-        Pattern pattern = Pattern.compile("(?![^{]*\\})(?=" + Pattern.quote(delimiter) + ")");
-        Matcher matcher = pattern.matcher(input);
-        int start = 0;
-        while (matcher.find()) {
-            String part = input.substring(start, matcher.end());
-            log.info(part.trim());
-            parts.add(part.trim());
-            start = matcher.end();
-        }
-        if (start < input.length()) {
-            parts.add(input.substring(start).trim());
-        }
-        return parts;
     }
 }
